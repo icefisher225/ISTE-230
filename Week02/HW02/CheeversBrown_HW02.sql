ad20c1d51c1c4b3f166a890906db1d71f0f61086
icefisher225 <ryan225360@gmail.com>
2022-09-08 16:54:23 -0400

    working on homework 02

---
+++ Week02/HW02/CheeversBrown_HW02.sql
@@ -0,0 +1,23 @@
+-- Ryan Cheevers-Brown
+-- HW 02 - ISTE230, Intro to DB
+
+-- Table: Item
+CREATE TABLE Item (
+    itemID VARCHAR(25) UNIQUE,
+    itemName VARCHAR(25),
+    iName VARCHAR(25),
+    street VARCHAR(25),
+    city VARCHAR(25), 
+    colors VARCHAR(25),
+    usState CHAR(2),
+    zipcode CHAR(5),
+    cost INT,
+    retailPrice INT,
+    notes VARCHAR(255),
+    descriptn VARCHAR(255), 
+    returnable CHAR(1),
+    perishable CHAR(1),
+    shelfQty INT UNSIGNED
+)
+
+DESCRIBE Item;
