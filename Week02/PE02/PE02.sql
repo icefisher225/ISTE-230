a4f6268f8af788a658cdb3327f1897dcac961a32
icefisher225 <ryan225360@gmail.com>
2022-09-09 15:16:44 -0400

    editing PE02

--- Week02/PE02/PE02.sql
+++ Week02/PE02/PE02.sql
@@ -9,10 +9,11 @@ USE PE02;
 
 CREATE TABLE Books (
     title           VARCHAR(50),
-    isbn13Number    VARCHAR(13)     UNIQUE,
+    isbn13Number    CHAR(13)     UNIQUE,
     author          VARCHAR(255),
-    numberOfPages   SMALLINT,
+    numberOfPages   SMALLINT UNSIGNED,
     releaseDate     DATE,
-);
+    CONSTRAINT Books_pk PRIMARY KEY (isbn13Number)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DESCRIBE Books;
