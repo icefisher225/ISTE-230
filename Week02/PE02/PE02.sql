9f661186fd095c644b5111789620fd8c3885de89
icefisher225 <ryan225360@gmail.com>
2022-09-09 15:08:44 -0400

    editing PE02

--- Week02/PE02/PE02.sql
+++ Week02/PE02/PE02.sql
@@ -1,5 +1,12 @@
 -- tables
 -- Table: Books
+
+DROP DATABASE IF EXISTS PE02;
+
+CREATE DATABASE PE02;
+
+USE PE02;
+
 CREATE TABLE Books (
     title           VARCHAR(50),
     isbn13Number    VARCHAR(13)     UNIQUE,
